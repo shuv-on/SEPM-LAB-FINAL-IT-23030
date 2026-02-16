219793be3056afbac506ac1dfddc87429c7019de
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2026-02-16 14:04:20 +0600

    CGPA section Added

--- Project-02/TestSpringJPA/src/main/java/com/example/TestSpringJPA/User.java
+++ Project-02/TestSpringJPA/src/main/java/com/example/TestSpringJPA/User.java
@@ -4,36 +4,43 @@ import jakarta.persistence.*;
 
 @Entity
 public class User {
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
     private String email;
+    private Double cgpa; 
 
-    // Getters and setters
+    // ID Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Name Getters and Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Email Getters and Setters
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // CGPA Getters and Setters 
+    public Double getCgpa() {
+        return cgpa;
+    }
+    public void setCgpa(Double cgpa) {
+        this.cgpa = cgpa;
+    }
 }
